8bd98efa8ec86363aca2818da3aaa00ea0652fe4
Fuad Izzudin <etge1234@gmail.com>
2021-06-12 13:15:10 +0700

    update base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://occ-monitoring.linkaja.com';
+$config['base_url'] = 'http://occ-monitoring.linkaja.com/';
 
 /*
 |--------------------------------------------------------------------------
